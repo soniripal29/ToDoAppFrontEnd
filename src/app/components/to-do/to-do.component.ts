be64ca7b19b0f46fa0fd90169bbd1f6e3d1dbb19
Nishtha Patel <patel6m4@uwindsor.ca>
2022-10-20 18:27:51 -0400

    Sprint 2 changes

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -9,9 +9,10 @@ import { AddTaskDialogComponent } from '../add-task-dialog/add-task-dialog.compo
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.css']
 })
-export class ToDoComponent implements OnInit,AfterViewInit {
+export class ToDoComponent implements OnInit, AfterViewInit {
 
   displayedColumns: string[] = ['task', 'description', 'status'];
+  displayedColumns_Completed: string[] = ['task', 'description', 'status', 'action'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator)
@@ -42,25 +43,25 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {task: "task1", description: 'task1', status: "Active"},
-  {task: "task2", description: 'task2', status: "Completed"},
-  {task: "task3", description: 'task3', status: "Active"},
-  {task: "task4", description: 'task4', status: "Completed"},
-  {task: "task5", description: 'task5', status: "Active"},
-  {task: "task6", description: 'task6', status: "Completed"},
-  {task: "task7", description: 'task7', status: "Active"},
-  {task: "task8", description: 'task8', status: "Completed"},
-  {task: "task9", description: 'task9', status: "Active"},
-  {task: "task10", description: 'task10', status: "Completed"},
-  {task: "task11", description: 'task11', status: "Active"},
-  {task: "task12", description: 'task12', status: "Completed"},
-  {task: "task13", description: 'task13', status: "Active"},
-  {task: "task14", description: 'task14', status: "Completed"},
-  {task: "task15", description: 'task15', status: "Active"},
-  {task: "task16", description: 'task16', status: "Completed"},
-  {task: "task17", description: 'task17', status: "Active"},
-  {task: "task18", description: 'task18', status: "Completed"},
-  {task: "task19", description: 'task19', status: "Active"},
-  {task: "task20", description: 'task20', status: "Completed"},
-  
+  { task: "task1", description: 'task1', status: "Active" },
+  { task: "task2", description: 'task2', status: "Completed" },
+  { task: "task3", description: 'task3', status: "Active" },
+  { task: "task4", description: 'task4', status: "Completed" },
+  { task: "task5", description: 'task5', status: "Active" },
+  { task: "task6", description: 'task6', status: "Completed" },
+  { task: "task7", description: 'task7', status: "Active" },
+  { task: "task8", description: 'task8', status: "Completed" },
+  { task: "task9", description: 'task9', status: "Active" },
+  { task: "task10", description: 'task10', status: "Completed" },
+  { task: "task11", description: 'task11', status: "Active" },
+  { task: "task12", description: 'task12', status: "Completed" },
+  { task: "task13", description: 'task13', status: "Active" },
+  { task: "task14", description: 'task14', status: "Completed" },
+  { task: "task15", description: 'task15', status: "Active" },
+  { task: "task16", description: 'task16', status: "Completed" },
+  { task: "task17", description: 'task17', status: "Active" },
+  { task: "task18", description: 'task18', status: "Completed" },
+  { task: "task19", description: 'task19', status: "Active" },
+  { task: "task20", description: 'task20', status: "Completed" },
+
 ];
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -32,7 +32,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialogModule } from '@angular/material/dialog'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 const materialModules = [
   CdkTreeModule,
@@ -68,7 +69,8 @@ const materialModules = [
   MatDatepickerModule,
   MatTooltipModule,
   MatNativeDateModule,
-  MatDialogModule
+  MatDialogModule,
+  MatSlideToggleModule
 ];
 @NgModule({
   imports: [
